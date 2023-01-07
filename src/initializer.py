c8c4f1f99446dc8834938ab04c75f5eddbac6a61
ehrenb <branden.a.ehrenreich@gmail.com>
2023-01-07 17:38:06 -0500

    init neo ogm

--- src/initializer.py
+++ src/initializer.py
@@ -1,4 +1,6 @@
-from pyorient.ogm import Graph, Config
+from datetime import datetime
+
+from neomodel import install_all_labels
 
 from machina.core.worker import Worker
 from machina.core.models import *
@@ -12,9 +14,44 @@ class Initializer(Worker):
     def __init__(self, *args, **kwargs):
         super(Initializer, self).__init__(*args, **kwargs)
 
-    def init_orientdb(self):
+    def init_neo4j(self):
 
+        self.logger.info("initializing neo4j models")
+        install_all_labels()
+
+        # TEST inputs
+
+        # ts = datetime.now()
+        # md5sum = 'test123'
+        # sha256sum = 'test456'
+        # type = 'testtype'
+        # size=12345
+
+        # art = Artifact(
+        #     md5=md5sum,
+        #     sha256=sha256sum,
+        #     ts=ts,
+        #     size=size,
+        #     type=type
+        # ).save()
+
+        # test = PNG(
+        #     md5=md5sum,
+        #     sha256=sha256sum,
+        #     ts=ts,
+        #     type=type,
+        #     size=size,
+        #     exif={'test':123}
+        # ).save()
         
-        self.logger.info("creating Nodes and Relationships")
-        self.graph.create_all(Node.registry)
-        self.graph.create_all(Relationship.registry)
+        # zip = Zip(
+        #     md5=md5sum,
+        #     sha256=sha256sum,
+        #     ts=ts,
+        #     type=type,
+        #     size=size
+        # ).save()
+
+        # extracts_rel = zip.extracts.connect(test).save()
+        # similar_rel = zip.similar.connect(test).save()
+        # retyped_rel = zip.retyped.connect(test).save()
